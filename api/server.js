6c0adafd16c8eba9703ff0b9d20e7b44a2f5d00b
Sid <siddhartha.mish2000@gmail.com>
2020-01-25 23:31:33 +0000

    moved twitter to server

--- api/server.js
+++ api/server.js
@@ -7,6 +7,15 @@ const authToken = config["twilio-auth-token"]
 console.log(authToken)
 const client = require('twilio')(accountSid, authToken);
 
+var Twitter = require('twitter');
+ 
+var twitterclient = new Twitter({
+	consumer_key: 'FFuhR8rr712gty5EXi5xzyssx',
+    consumer_secret: '0UKAAa1mVJY23hQjjnMO2L0qnQoKhrC2GRx6Irp0A8zYBEpZ3Z',
+    access_token_key: '1221057192192880641-W0I7NzE1eV3s09aZkC2VXWNGu60wuK',
+    access_token_secret: 'jbUBQEfkbolyiJqHhQfmNEmsW1sh99dmFP6wlMXgEGBGr'
+});
+
 var express = require("express");
 var num = '+447871239341';
 
@@ -20,7 +29,7 @@ app.get("/threaten/:threatID", function(req,res){
     res.send("threat level: "+req.params['threatID'])
 	
 	switch(req.params['threatID']){
-		case '3':
+		case '4':
 			client.messages
 			  .create({
 				 body: 'Monkey loves you',
@@ -29,7 +38,7 @@ app.get("/threaten/:threatID", function(req,res){
 			   })
 			  .then(message => console.log(message.sid));
 			break;
-		case '4':
+		case '3':
 			client.calls
 			  .create({
 				 twiml: '<Response><Say voice="alice">Hello, friend.</Say><Play></Play><Say voice="alice">Goodbye, friend.</Say></Response>',
@@ -38,12 +47,22 @@ app.get("/threaten/:threatID", function(req,res){
 			   })
 			  .then(call => console.log(call.sid));
 			break;
+		
+		case '2':
+			twitterclient.post('statuses/update', {status: 'Look alive sunshine'}, function(error, tweet, response) {
+				if (!error) {
+				console.log(tweet);
+				} else {
+					console.log("there was an issue");
+				}
+			});
+			break;
+		
+		case '1':
+			console.log("making coffee");
+			break;
+		
 	}
 })
-
-app.get("/twitter/:threatID", function(req,res){
-    res.send("threat level: "+req.paramas['threatID'])
-})
-
 
 console.log("server listening on port: "+port)

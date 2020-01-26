e66ef9182c83fd78c1f864e540c958fc113590ec
DummybugStudios <siddhartha.mish2000@gmail.com>
2020-01-26 11:07:57 +0000

    fixed audio ending

--- api/server.js
+++ api/server.js
@@ -23,6 +23,8 @@ var num = '+447871239341';
 
 app = express();
 
+app.use(express.static('public'))
+
 // TODO:  ffs please add config
 port = 80;
 
@@ -44,7 +46,7 @@ app.get("/threaten/:threatID", function(req,res){
 		case '3':
 			client.calls
 			  .create({
-				 twiml: '<Response><Say voice="alice">Hello, friend.</Say><Play>http://lookalivesunshine.tech/resources/interference.wav</Play><Say voice="alice">Goodbye, friend.</Say></Response>',
+				 twiml: '<Response><Say voice="alice">Hello, friend.</Say><Play>http://lookalivesunshine.tech/resources/interference.wav</Play></Response>',
 				 from: '+12014743543',
 				 to: num
 			   })

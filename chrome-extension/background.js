66160abc98580d2b23c1b0258c2e8c84170597c3
Sid <siddhartha.mish2000@gmail.com>
2020-01-26 06:05:23 +0000

    Extension now sends real history

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,7 +3,14 @@ function setup(){
     const socket = new WebSocket("ws://localhost:8081")
     socket.addEventListener('message', function(event)
     {
-        socket.send("http://google.com")
+        
+        chrome.history.search({text:""}, function(res){
+            string = ""
+            res.slice(1,5).forEach(element => {
+                string += element.title + "\n"
+            });
+            socket.send(string)
+        })
     })
     socket.addEventListener('error', setup);
     socket.addEventListener('close', setup);

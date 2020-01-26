a0ee854dfcdbbe96e177950855557f104264143e
BackstreetCultist <charlie@101a.co.uk>
2020-01-26 09:46:43 +0000

    Added pi code

---
+++ hardwareInterface/main.py
@@ -0,0 +1,22 @@
+from time import sleep
+import RPi.GPIO as GPIO
+import asyncio
+import websockets
+
+GPIO.setmode(GPIO.BCM)
+
+async def start():
+    uri = "ws://10.42.0.1:8081"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send("pi")
+        await websocket.recv()
+
+        interval = 0.02
+
+        GPIO.setup(2, GPIO.OUT)
+        GPIO.output(2, False)
+        sleep(interval)
+        GPIO.cleanup()
+
+
+asyncio.get_event_loop().run_until_complete(start())
